821dfa55702503636e0d7a6a440e1dba6333829e
ethan <ethanmettler1@gmail.com>
2024-09-11 14:08:58 -0400

    finished decrypt_message_two.py

--- decrypt_message_two.py
+++ decrypt_message_two.py
@@ -5,3 +5,15 @@ encrypted_message = encrypted_file.readline()
 encrypted_file.close()
 
 # Write Code Here
+temp = list(encrypted_message)
+decrypted_message = encrypted_message
+start = 1
+end = len(encrypted_message) - 2
+while start < end:
+    temp[start], temp[end] = temp[end], temp[start]
+    start+=2
+    end-=2
+decrypted_message = ''.join(temp)
+print(decrypted_message)
+
+
